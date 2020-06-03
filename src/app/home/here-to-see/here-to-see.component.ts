f41a2707a5cd2034d35bd1c9afc9ae842d271089
Danish <80050@EIA095.exalca.corp>
2020-06-03 15:56:04 +0530

    Init

---
+++ src/app/home/here-to-see/here-to-see.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-here-to-see',
+  templateUrl: './here-to-see.component.html',
+  styleUrls: ['./here-to-see.component.scss'],
+})
+export class HereToSeeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,16 +1,21 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HomePage } from './home.page';
+import { HereToSeeComponent } from "./here-to-see/here-to-see.component";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HomePage } from "./home.page";
 
 const routes: Routes = [
   {
-    path: '',
+    path: "",
     component: HomePage,
-  }
+  },
+  {
+    path: "heretosee",
+    component: HereToSeeComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class HomePageRoutingModule {}
