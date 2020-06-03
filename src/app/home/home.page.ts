e760d1b15c698e0b20526840abcdb342e57dddc7
Danish <80050@EIA095.exalca.corp>
2020-06-03 17:10:27 +0530

    Added Here to see

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,17 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-home",
   templateUrl: "home.page.html",
   styleUrls: ["home.page.scss"],
 })
-export class HomePage {
+export class HomePage implements OnInit {
   dateTime = new Date();
 
-  constructor() {}
+  constructor(private route: Router) {}
+
+  ngOnInit() {
+    //this.route.navigateByUrl("/home/heretosee");
+  }
 }
